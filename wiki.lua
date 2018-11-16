6a28e647fb5470aca14c2408f1960a510a615a9c
Wintermourn <Wintermourn@users.noreply.github.com>
2018-11-16 11:01:44 -0600

    Update wiki.lua

--- wiki.lua
+++ wiki.lua
@@ -2,6 +2,14 @@ local wiki = {}
 
 wiki.hoopsabot = "It's me!"
 wiki.help = [[```AsciiDoc
-*test*```]]
+*HOOPSABOT*
+A Work in progress, poorly made discord bot built for fun.
+
+Throughout this wiki definition, every `h.' should be replaced with the server's prefix, which is `h.' by default.
+
+*COMMANDS*
+
+To view the list of commands, type `h.commands'. However, this command does not list every command,]]..
+[[ and some you may have to figure out on your own or from others.```]]
 
 return wiki
