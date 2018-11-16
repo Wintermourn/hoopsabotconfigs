0e4d652672a8da4c9893a3b36ad763547dab4aa9
scamantus <39003297+scamantus@users.noreply.github.com>
2018-11-16 17:51:59 +0000

    New Entry: Rep

--- wiki.lua
+++ wiki.lua
@@ -11,7 +11,8 @@ Throughout this wiki definition, every `h.' should be replaced with the server's
 
 To view the list of commands, type `h.commands'. However, this command does not list every command,]]..
 [[ and some you may have to figure out on your own or from others.```]]
-wiki.test = [[```diff
-Testing! ```]]
-
+wiki.rep = [[```AsciiDoc
+*REP*
+A command that lets you give a rep point once every 24 hours. 
+Use h.rep on its own to see if you can give a rep or not (or how long it will take for you to be able to do so), and h.rep along with mentioning a user to give the user a rep point. ```]]
 return wiki
